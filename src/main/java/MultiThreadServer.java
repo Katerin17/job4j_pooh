88d65a1d9b0a481a82958123c9d7717667efcc2f
Ekaterina Mironenko <Krivets86@mail.ru>
2021-01-28 22:21:54 +0300

    1. Техническое задание - проект "Pooh JMS" [#433187] changed architecture

--- src/main/java/MultiThreadServer.java
+++ src/main/java/MultiThreadServer.java
@@ -1,36 +1,46 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.http.HttpRequest;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
 
+@SuppressWarnings("checkstyle:LineLength")
 public class MultiThreadServer {
     private static int size = Runtime.getRuntime().availableProcessors();
     private static ExecutorService executor = Executors.newFixedThreadPool(size);
+    private static ConcurrentLinkedQueue<String> queue = new ConcurrentLinkedQueue<>();
 
-    public static void main(String[] args) {
-        try (ServerSocket server = new ServerSocket(3345)) {
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket server = new ServerSocket(8080)) {
             while (!server.isClosed()) {
                 Socket client = server.accept();
                 executor.execute(() -> {
-                    try {
-                        Connection connection = new Connection(client);
-                        while (!client.isClosed()) {
-                            HttpRequest entry = connection.receive();
-                            HttpClientRequest httpClientRequest = new HttpClientRequest();
-                            String s = httpClientRequest.getResponse(entry);
+                    try (BufferedReader input = new BufferedReader(new InputStreamReader(client.getInputStream(), StandardCharsets.UTF_8));
+                         PrintWriter output = new PrintWriter(client.getOutputStream())) {
+                        if (input.ready()) {
+                            String s = input.lines().collect(Collectors.joining());
+                            System.out.println(s);
+                            if (s.startsWith("POST")) {
+                                if (s.matches("POST /queue/.+")) {
+                                    queue.offer(s);
+                                }
+                            } else if (s.startsWith("GET")) {
+                                if (s.matches("GET /queue/.+")) {
+                                    queue.poll();
+                                }
+                            }
                         }
-                        connection.close();
-                        System.out.println("Closing connections...");
-                    } catch (Exception e) {
+                        output.println("HTTP/1.1 200 OK");
+                        output.println("Content-Type: text/html; charset=utf-8");
+                        output.println();
+                        output.println("<p>The request accepted.</p>");
+                        output.flush();
+                    } catch (IOException e) {
                         e.printStackTrace();
                     }
                 });
             }
-            executor.shutdown();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
